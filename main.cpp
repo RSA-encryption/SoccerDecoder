b0e1952be715e8ccfb29cb74305eaa4c0e75d788
︹_/﹀\_︿  ︺\/ <49093537+RSA-encryption@users.noreply.github.com>
2021-06-21 15:25:15 +0200

    Argv support

--- main.cpp
+++ main.cpp
@@ -6,57 +6,66 @@
 #include <assert.h>
 #include <thread>
 #include <atomic>
+#include <algorithm>
 
 #include "headers/ErlangDistribution.hpp"
 #include "headers/TeamMatch.hpp"
 #include "headers/Team.hpp"
 #include "headers/Player.hpp"
 
-#define TOTAL_MATCHES 50000
+#define DEFAULT_MATCHES 1000
 
 unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 auto TCount = std::thread::hardware_concurrency();
-std::atomic<int> aw {0}, dw{0}, bw{0};
+std::atomic<int> aw{0}, dw{0}, bw{0};
 std::chi_squared_distribution<float> uniDub(1.0f);
 std::default_random_engine gen(seed);
 
 void func(std::vector<FieldUnit::Player> APlayers, std::vector<FieldUnit::Player> BPlayers, int chunk)
 {
     srand(time(NULL));
-    for (size_t i = 0; i < chunk; i++)
-    {
+    for (size_t i = 0; i < chunk; i++){
         Match::Team a("Cheese Cakes", 0.35f, 3.0f, 2.0f, 0.0f, APlayers, FieldUnit::Player(8.0f, uniDub(gen), "Rofl", FieldUnit::Player::GOALKEEPER), Match::Team::Formations::_4_4_2);
         Match::Team b("Goed God", 0.2f, 2.0f, 1.0f, 0.0f, BPlayers, FieldUnit::Player(9.0f, uniDub(gen), "Lmao", FieldUnit::Player::GOALKEEPER), Match::Team::Formations::_4_4_2);
         Match::FaceOff PlayOff(a, b);
 
         PlayOff.Simulate();
 
-        if (PlayOff.ScoreA > PlayOff.ScoreB)
-            aw++;
-        else if (PlayOff.ScoreA < PlayOff.ScoreB)
-            bw++;
-        else
-            dw++;
+        if (PlayOff.ScoreA > PlayOff.ScoreB) aw++;
+        else if (PlayOff.ScoreA < PlayOff.ScoreB) bw++;
+        else dw++;
     }
 }
 
-std::vector<int> chunks()
+std::vector<int> chunks(int count)
 {
     std::vector<int> chunk;
     int total = 0;
-    for (size_t i = 0; i < TCount; i++)
-    {
-        chunk.push_back((int)std::floor(TOTAL_MATCHES) / (TCount + 1));
-        total += (int)std::floor(TOTAL_MATCHES) / (TCount + 1);
+    for (size_t i = 0; i < TCount; i++){
+        chunk.push_back((int)std::floor(count) / (TCount + 1));
+        total += (int)std::floor(count) / (TCount + 1);
     }
-    if ((int)std::floor(TOTAL_MATCHES) - total != 0)
-        chunk.push_back((int)std::floor(TOTAL_MATCHES) - total);
+    if ((int)std::floor(count) - total != 0)
+        chunk.push_back((int)std::floor(count) - total);
     return chunk;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::vector<int> chunk = chunks();
+    int count;
+    std::string s(argv[1]);
+    try{
+        if (argc > 0) count = std::stoi(s);
+        if (count < 0) count = DEFAULT_MATCHES;
+    }
+    catch (const std::invalid_argument &e){
+        std::cerr << "Error : invalid_argument must be a integer" << std::endl;
+        exit(-1);
+    }
+    catch (const std::out_of_range &e){
+        std::cerr << "Error : out_of_range" << std::endl;
+        exit(-1);
+    }
     std::vector<std::thread> Threads;
     std::vector<FieldUnit::Player> APlayers{
         FieldUnit::Player(7.5f, uniDub(gen), "Kanker", FieldUnit::Player::MIDFIELDER),
@@ -82,22 +91,26 @@ int main()
         FieldUnit::Player(7.0f, uniDub(gen), "Epaaaaaaaaic", FieldUnit::Player::DEFENDER),
         FieldUnit::Player(7.5f, uniDub(gen), "Epzzzzzzzzzzic", FieldUnit::Player::DEFENDER)};
     auto start = std::chrono::high_resolution_clock::now();
-    for (auto &var : chunks())
+    if (!(std::stoi(s) < TCount * 2))
     {
-        Threads.push_back(std::thread([&]
-                                      { func(APlayers, BPlayers, var); }));
-    }
-    for (auto &t : Threads)
-    {
-        if (t.joinable()) t.join();
-    }
+        for (auto &var : chunks(count))
+        {
+            Threads.push_back(std::thread([&]
+                                          { func(APlayers, BPlayers, var); }));
+        }
+        for (auto &t : Threads)
+        {
+            if (t.joinable())
+                t.join();
+        }
+    } else func(APlayers, BPlayers, count);
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
     std::cout << "******************score*********************" << std::endl;
     std::cout << "A wins : " << aw << std::endl;
     std::cout << "B wins : " << bw << std::endl;
     std::cout << "Draws : " << dw << std::endl;
-    std::cout << "Total matches : " << TOTAL_MATCHES << " Runtime: " << duration.count() << " miliseconds" << std::endl;
+    std::cout << "Total matches : " << count << " Runtime: " << duration.count() << " miliseconds" << std::endl;
     std::cout << "******************score*********************" << std::endl;
     return 0;
 }
